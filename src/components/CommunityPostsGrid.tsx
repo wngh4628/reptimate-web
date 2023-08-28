001a0ede78ef687f28aa47d3889f61c5dcef986b
YOOSOO <firekiser@naver.com>
2023-08-28 16:00:22 +0900

    components folder create

---
+++ src/components/CommunityPostsGrid.tsx
@@ -0,0 +1,15 @@
+import { Adpotion } from "@/service/adoption";
+import PostCard from "./home-post/PostCard";
+
+type Props = { posts: Adpotion[] };
+export default function CommunityHomePostsGird({ posts }: Props) {
+  return (
+    <ul className="pl-10 pr-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+      {posts.map((post) => (
+        <li key={post.userIdx}>
+          <PostCard post={post} />
+        </li>
+      ))}
+    </ul>
+  );
+}
