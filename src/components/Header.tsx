0373f55ecd3061241e7a3c556194646d91393096
YOOSOO <firekiser@naver.com>
2023-08-30 17:26:26 +0900

    adoption with login/logout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { Mobile, PC } from "./ResponsiveLayout";
 import { useEffect, useState } from "react";
-import { useRecoilState } from 'recoil';
+import { useRecoilState } from "recoil";
 import { userAtom } from "@/recoil/user";
 
 export default function Header() {
@@ -14,28 +14,26 @@ export default function Header() {
   const [isLogin, isSetLogin] = useState(false);
   const [accessToken, setAccessToken] = useRecoilState(userAtom);
 
-  const pathName = usePathname();
-
   useEffect(() => {
-   handleLogin();
-  }, [pathName])
+    handleLogin();
+  }, [pathName]);
 
   const handleLogin = () => {
-    const storedData = localStorage.getItem('recoil-persist');
-      if (storedData) {
-        const userData = JSON.parse(storedData);
-        if (userData.USER_DATA.accessToken != null) {
-          const accessToken = userData.USER_DATA.accessToken;
-          isSetLogin(true);
-        }
+    const storedData = localStorage.getItem("recoil-persist");
+    if (storedData) {
+      const userData = JSON.parse(storedData);
+      if (userData.USER_DATA.accessToken != null) {
+        const accessToken = userData.USER_DATA.accessToken;
+        isSetLogin(true);
       }
+    }
   };
   const handleLogout = () => {
-    localStorage.removeItem('recoil-persist');
+    localStorage.removeItem("recoil-persist");
     isSetLogin(false);
     router.refresh();
   };
-  
+
   const communityPathnames = [
     "/",
     "/community/used-deal",
@@ -49,7 +47,10 @@ export default function Header() {
       <PC>
         <div className="flex justify-end pr-10 pt-5 gap-2 font-bold">
           {isLogin ? (
-            <button  className="group hover:text-main-color" onClick={handleLogout}>
+            <button
+              className="group hover:text-main-color"
+              onClick={handleLogout}
+            >
               로그아웃
             </button>
           ) : (
@@ -70,10 +71,7 @@ export default function Header() {
             </div>
           </Link>
           <nav className="flex gap-4 font-bold">
-            <Link
-              href="/"
-              className={` group hover:text-main-color`}
-            >
+            <Link href="/" className={` group hover:text-main-color`}>
               COMMUNITY
             </Link>
             <Link
--- src/recoil/user.ts
+++ src/recoil/user.ts
@@ -4,9 +4,9 @@ import { recoilPersist } from "recoil-persist";
 const { persistAtom } = recoilPersist(); // 페이지가 변경되더라도 상태관리를 유지하기 위해 사용된다.
 
 interface loginResponse {
-  status : number,
-  message : string,
-  result : User
+  status: number;
+  message: string;
+  result: User;
 }
 interface User {
   accessToken: string;
@@ -19,13 +19,11 @@ export interface UserData {
     accessToken: string;
     refreshToken: string;
     id: number;
-  }
+  };
 }
-
 
 export const userAtom = atom<User | null>({
   key: "USER_DATA",
   default: null,
   effects_UNSTABLE: [persistAtom],
 });
-
