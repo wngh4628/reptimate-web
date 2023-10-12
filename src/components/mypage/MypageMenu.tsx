cd619233e82c1027d32594513ad6cd725cd3642e
root <root@ip-172-31-35-18.ap-northeast-2.compute.internal>
2023-10-12 12:20:42 +0000

    mypage

--- src/components/mypage/MypageMenu.tsx
+++ src/components/mypage/MypageMenu.tsx
@@ -42,57 +42,6 @@ export default function MypageMenu() {
   };
 
   return (
-<<<<<<< HEAD
-    <div className="flex">
-      <PC>
-        <div className="flex w-[235px] drop-shadow-md">
-          <nav className="flex flex-col font-bold w-full">
-            <Link
-              href="/my"
-              className={`${
-<<<<<<< HEAD
-                pathName === "/my" ? "" : "hover:text-[#6D71E6]"
-              } group  border-b-[1px] h-[60px] flex items-center pl-2 bg-[#6D71E6] text-white`}
-=======
-                pathName === "/my" ? "bg-[#6D71E6] text-white" : "hover:text-[#6D71E6] bg-white"
-              } group  border-b-[1px] h-[60px] flex items-center pl-2`}
->>>>>>> 600dc0a5f8207f52e5ab4a33ffa1b1fa1501a7a8
-            >
-              내 정보 수정
-            </Link>
-            <Link
-              href="/my/board"
-              className={`${
-<<<<<<< HEAD
-                pathName === "/my/board" ? "text-white bg-[#6D71E6]" : "hover:text-[#6D71E6]"
-              } group  bg-white border-b-[1px] h-[60px] flex items-center pl-2`}
-=======
-                pathName === "/my/board" ? "bg-[#6D71E6] text-white" : "hover:text-[#6D71E6] bg-white"
-              } group border-b-[1px] h-[60px] flex items-center pl-2`}
->>>>>>> 600dc0a5f8207f52e5ab4a33ffa1b1fa1501a7a8
-            >
-              내가 쓴 글
-            </Link>
-            <Link
-              href="/my/auction"
-              className={`${
-                pathName === "/my/auction" ? "text-white bg-[#6D71E6]" : "hover:text-[#6D71E6] bg-white"
-              } group border-b-[1px] h-[60px] flex items-center pl-2`}
-            >
-              내 경매
-            </Link>
-            <Link
-              href="/my/bookmark"
-              className={`${
-                pathName === "/my/bookmark" ? "text-white bg-[#6D71E6]" : "hover:text-[#6D71E6] bg-white"
-              } group  h-[60px] flex items-center pl-2`}
-            >
-              북마크
-            </Link>
-          </nav>
-        </div>
-      </PC>
-=======
     <>
       <div className="flex">
         <PC>
@@ -141,50 +90,10 @@ export default function MypageMenu() {
             </nav>
           </div>
         </PC>
->>>>>>> 71ed7f408a3395796cf97eeea84a219f7e3cb3f2
 
         <Mobile>
           <div className="flex mb-[25px] drop-shadow-md items-center justify-center content-center m-auto w-full border-[1px]">
             <nav className="flex font-bold w-full">
-<<<<<<< HEAD
-                <Link
-                  href="/my"
-                  className={`${
-<<<<<<< HEAD
-                    pathName === "/my" ? "" : "hover:text-[#6D71E6] "
-                  } group  h-[50px] flex items-center justify-center text-center px-5 border-r-[1px] w-1/4 bg-[#6D71E6] text-white`}
-=======
-                    pathName === "/my" ? "bg-[#6D71E6] text-white" : "hover:text-[#6D71E6] "
-                  } group  h-[50px] flex items-center justify-center text-center px-2 border-r-[1px] w-1/4 text-[13px]`}
->>>>>>> 600dc0a5f8207f52e5ab4a33ffa1b1fa1501a7a8
-                >
-                  내 정보 수정
-                </Link>
-                <Link
-                  href="/my/board"
-                  className={`${
-                    pathName === "/my/board" ? "bg-[#6D71E6] text-white" : "hover:text-[#6D71E6]"
-                  } group  h-[50px] flex items-center justify-center text-center px-2 border-r-[1px] w-1/4 text-[13px]`}
-                >
-                  내가 쓴 글
-                </Link>
-                <Link
-                  href="/my/free"
-                  className={`${
-                    pathName === "/my/auction" ? "bg-[#6D71E6] text-white" : "hover:text-[#6D71E6]"
-                  } group  h-[50px] flex items-center justify-center text-center px-5 border-r-[1px] w-1/4 text-[13px]`}
-                >
-                  내 경매
-                </Link>
-                <Link
-                  href="/my/bookmark"
-                  className={`${
-                    pathName === "/my/bookmark" ? "bg-[#6D71E6] text-white" : "hover:text-[#6D71E6] "
-                  } group  h-[50px] flex items-center justify-center text-center px-5 w-1/4 text-[13px]`}
-                >
-                  북마크
-                </Link>
-=======
               <Link
                 href="/my"
                 className={`${
@@ -225,7 +134,6 @@ export default function MypageMenu() {
               >
                 북마크
               </Link>
->>>>>>> 71ed7f408a3395796cf97eeea84a219f7e3cb3f2
             </nav>
           </div>
         </Mobile>
