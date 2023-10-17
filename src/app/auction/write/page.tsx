2dd5685b90793b7af0775eebbf1bf07f1144eab9
YOOSOO <firekiser@naver.com>
2023-10-17 16:15:22 +0900

    edit DndProvider

--- src/app/auction/write/page.tsx
+++ src/app/auction/write/page.tsx
@@ -6,23 +6,21 @@ import { HTML5Backend } from "react-dnd-html5-backend";
 import React from "react";
 import AuctionWrite from "@/components/auction/AuctionWrite";
 import { TouchBackend } from "react-dnd-touch-backend";
+import { Mobile, PC } from "@/components/ResponsiveLayout";
 
 export default function AuctionWritePage() {
-  const [isMobile, setIsMobile] = React.useState(false);
-
-  React.useEffect(() => {
-    const mediaQuery = window.matchMedia("(max-width: 768px)");
-    const isMobileDevice = mediaQuery.matches;
-    setIsMobile(isMobileDevice);
-  }, []);
-
-  const DndProviderComponent = isMobile ? TouchBackend : HTML5Backend;
-
   return (
     <div>
-      <DndProvider backend={DndProviderComponent}>
-        <AuctionWrite />
-      </DndProvider>
+      <PC>
+        <DndProvider backend={HTML5Backend}>
+          <AuctionWrite />
+        </DndProvider>
+      </PC>
+      <Mobile>
+        <DndProvider backend={TouchBackend}>
+          <AuctionWrite />
+        </DndProvider>
+      </Mobile>
     </div>
   );
 }
--- src/app/community/adoption/write/page.tsx
+++ src/app/community/adoption/write/page.tsx
@@ -6,24 +6,22 @@ import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import React from "react";
 import { TouchBackend } from "react-dnd-touch-backend";
+import { Mobile, PC } from "@/components/ResponsiveLayout";
 
 export default function AdoptionWritePage() {
-  const [isMobile, setIsMobile] = React.useState(false);
-
-  React.useEffect(() => {
-    const mediaQuery = window.matchMedia("(max-width: 768px)");
-    const isMobileDevice = mediaQuery.matches;
-    setIsMobile(isMobileDevice);
-  }, []);
-
-  const DndProviderComponent = isMobile ? TouchBackend : HTML5Backend;
-
   return (
     <div>
       <CommunityMenu />
-      <DndProvider backend={DndProviderComponent}>
-        <AdoptionWrite />
-      </DndProvider>
+      <PC>
+        <DndProvider backend={HTML5Backend}>
+          <AdoptionWrite />
+        </DndProvider>
+      </PC>
+      <Mobile>
+        <DndProvider backend={TouchBackend}>
+          <AdoptionWrite />
+        </DndProvider>
+      </Mobile>
     </div>
   );
 }
