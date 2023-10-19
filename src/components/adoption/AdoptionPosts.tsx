79f7358fd92c07d17942847400e3439671ecb56b
YOOSOO <firekiser@naver.com>
2023-10-19 18:52:09 +0900

    edit layout design

--- src/components/adoption/AdoptionPosts.tsx
+++ src/components/adoption/AdoptionPosts.tsx
@@ -147,7 +147,7 @@ export default function AdoptionPosts() {
 
     return (
       <section>
-        <ul className="mt-10 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4">
+        <ul className="mt-5 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4">
           {itemlist.map((post) => (
             <li key={post.idx}>
               <PostCard post={post} />
