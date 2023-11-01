d12f857e8d2b3e8041ce5b188a23ab9f629346ac
YOOSOO <firekiser@naver.com>
2023-11-01 15:29:28 +0900

    header cookie test

--- src/components/ask/AskPosts.tsx
+++ src/components/ask/AskPosts.tsx
@@ -20,49 +20,6 @@ export default function AskPosts() {
   const setUser = useSetRecoilState(userAtom);
   const setIsLoggedIn = useSetRecoilState(isLoggedInState);
 
-  function getCookie(name: string) {
-    const value = "; " + document.cookie;
-    const parts = value.split("; " + name + "=");
-    if (parts.length == 2) {
-      const cookieValue = parts.pop()?.split(";").shift();
-      try {
-        const cookieObject = JSON.parse(cookieValue || "");
-        return cookieObject;
-      } catch (error) {
-        console.error("Error parsing JSON from cookie:", error);
-        return null;
-      }
-    }
-  }
-
-  useEffect(() => {
-    // 안드로이드 웹뷰를 통해 접속한 경우에만 실행됩니다.
-    const myAppCookie = getCookie("myAppCookie");
-
-    if (myAppCookie !== undefined) {
-      console.log(myAppCookie);
-      const accessToken = myAppCookie.accessToken;
-      const idx = parseInt(myAppCookie.idx || "", 10) || 0;
-      const refreshToken = myAppCookie.refreshToken;
-      const nickname = myAppCookie.nickname;
-      const profilePath = myAppCookie.profilePath;
-
-      console.log("accessToken: " + accessToken);
-      console.log("idx: " + idx);
-      console.log("refreshToken: " + refreshToken);
-      console.log("nickname: " + nickname);
-      console.log("profilePath: " + profilePath);
-      setUser({
-        accessToken: accessToken || "",
-        refreshToken: refreshToken || "",
-        idx: idx || 0,
-        profilePath: profilePath || "",
-        nickname: nickname || "",
-      });
-      setIsLoggedIn(true);
-    }
-  }, []);
-
   const options = {
     threshold: 1.0,
   };
