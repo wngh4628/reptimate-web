d0a5e20afdc8bb989665a201edbc340b8e3ab000
rlaryd <rlaryd1822@gmail.com>
2023-11-09 13:42:30 +0900

    23110900

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -8,23 +8,38 @@ interface VideoPlayerProps {
 
 const VideoPlayer: React.FC<VideoPlayerProps> = ({ src, type }) => {
   const videoRef = useRef<HTMLVideoElement | null>(null);
+  const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
 
   useEffect(() => {
-    if (type === "m3u8" && Hls.isSupported()) {
-      const hls = new Hls();
-      hls.loadSource(src);
-      hls.attachMedia(videoRef.current!); // Force conversion to a definite value
+
+    if (isSafari) {
+      if (type === "m3u8" && Hls.isSupported()) {
+        const hls = new Hls();
+        hls.loadSource(src);
+        hls.attachMedia(videoRef.current!); // Force conversion to a definite value
+      }
     }
   }, [src, type]);
 
   // Change to use HLS player only when the type is 'm3u8'
-  return type === "m3u8" ? (
-    <div className="absolute inset-0 w-full h-full object-contain bg-black flex justify-center items-center">
-      <video ref={videoRef} controls playsInline></video>
-    </div>
-  ) : (
+
+  if (type === "m3u8" && isSafari) {
+    return (
+      <video src={src} controls playsInline />
+    );
+  } else if (type === "m3u8") {
+    return (
+      <div className="absolute inset-0 w-full h-full object-contain bg-black flex justify-center items-center">
+        <video ref={videoRef} controls playsInline></video>
+      </div>
+    );
+  } else {
     <video src={src} controls playsInline/>
-  );
+  }
+
+  
+
+
 };
 
 export default VideoPlayer;
