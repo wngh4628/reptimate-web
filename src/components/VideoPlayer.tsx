a3c4b5addfa80ee8052efe7c7b471120362487f0
rlaryd <rlaryd1822@gmail.com>
2023-11-09 15:17:11 +0900

    fix: VideoPlayer 사파리 수정

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -8,33 +8,52 @@ interface VideoPlayerProps {
 
 const VideoPlayer: React.FC<VideoPlayerProps> = ({ src, type }) => {
   const videoRef = useRef<HTMLVideoElement | null>(null);
-  const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+
 
   useEffect(() => {
 
-    if (isSafari) {
-      if (type === "m3u8" && Hls.isSupported()) {
-        const hls = new Hls();
-        hls.loadSource(src);
-        hls.attachMedia(videoRef.current!); // Force conversion to a definite value
+
+
+
+      if (type === "m3u8" && videoRef.current) {
+      // Check if it's iOS Safari
+      const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+
+      if (isSafari) {
+        // Use native HLS support for iOS Safari
+        videoRef.current.src = src;
+      } else {
+        // Use hls.js for other browsers
+        const Hls = require("hls.js");
+        if (Hls.isSupported()) {
+          const hls = new Hls();
+          hls.loadSource(src);
+          hls.attachMedia(videoRef.current);
+        }
       }
     }
+
+      // if (type === "m3u8" && Hls.isSupported()) {
+      //   const hls = new Hls();
+      //   hls.loadSource(src);
+      //   hls.attachMedia(videoRef.current!); // Force conversion to a definite value
+      // }
   }, [src, type]);
 
   // Change to use HLS player only when the type is 'm3u8'
 
-  if (type === "m3u8" && isSafari) {
-    return (
-      <video src={src} controls playsInline />
-    );
-  } else if (type === "m3u8") {
+  
+  if (type === "m3u8") {
     return (
       <div className="absolute inset-0 w-full h-full object-contain bg-black flex justify-center items-center">
         <video ref={videoRef} controls playsInline></video>
       </div>
     );
   } else {
-    <video src={src} controls playsInline/>
+    console.log("111")
+    return (
+      <video src={src} controls playsInline />
+    );
   }
 
   
