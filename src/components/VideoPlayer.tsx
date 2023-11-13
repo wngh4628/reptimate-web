b43c8562faab74502b8c6f240ca79f21b7ba882e
YOOSOO <firekiser@naver.com>
2023-11-13 17:35:42 +0900

    edit video

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import Hls from "hls.js";
 
 interface VideoPlayerProps {
@@ -9,27 +9,26 @@ interface VideoPlayerProps {
 const VideoPlayer: React.FC<VideoPlayerProps> = ({ src, type }) => {
   const videoRef = useRef<HTMLVideoElement | null>(null);
 
-
   useEffect(() => {
-
     if (type === "m3u8" && videoRef.current) {
-      const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
-      const isAppleWebKit = /^((?!chrome|android).)*AppleWebKit/i.test(navigator.userAgent);
+      const isSafari = /^((?!chrome|android).)*safari/i.test(
+        navigator.userAgent
+      );
+      const isAppleWebKit = /^((?!chrome|android).)*AppleWebKit/i.test(
+        navigator.userAgent
+      );
       if (isSafari || isAppleWebKit) {
         videoRef.current.src = src;
         videoRef.current.setAttribute("playsinline", "true");
-      } else {
-        const Hls = require("hls.js");
-        if (Hls.isSupported()) {
-          const hls = new Hls();
-          hls.loadSource(src);
-          hls.attachMedia(videoRef.current);
-        }
+      }
+      if (type === "m3u8" && Hls.isSupported()) {
+        const hls = new Hls();
+        hls.loadSource(src);
+        hls.attachMedia(videoRef.current!); // Force conversion to a definite value
       }
     }
-
   }, [src, type]);
-    
+
   if (type === "m3u8") {
     return (
       <div className="absolute inset-0 w-full h-full object-contain bg-black flex justify-center items-center">
@@ -37,14 +36,8 @@ const VideoPlayer: React.FC<VideoPlayerProps> = ({ src, type }) => {
       </div>
     );
   } else {
-    return (
-      <video src={src} controls playsInline />
-    );
+    return <video src={src} controls playsInline />;
   }
-
-  
-
-
 };
 
 export default VideoPlayer;
