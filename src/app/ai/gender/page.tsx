df4737b8a37287f0c6f1f2311ba1fae194857926
YOOSOO <firekiser@naver.com>
2023-12-03 15:25:27 +0900

    edit layout design

--- src/app/ai/gender/page.tsx
+++ src/app/ai/gender/page.tsx
@@ -4,8 +4,8 @@ export default function GenderDiscriminationPage() {
   return (
     <div>
       <AiMenu />
-      <div className="max-w-screen-sm mx-auto">
-        <h2 className="text-3xl font-bold pt-5">{'암수 구분'}</h2>
+      <div className="max-w-screen-sm mx-auto mt-20">
+        <h2 className="text-3xl font-bold pt-5">{"암수 구분"}</h2>
       </div>
     </div>
   );
