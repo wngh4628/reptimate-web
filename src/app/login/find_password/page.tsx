3c89771b539d76b045aad5a4c60277aa3c2c10c7
rlaryd <rlaryd1822@gmail.com>
2023-12-04 18:21:13 +0900

    Feat: 비밀번호 찾기 페이지 제작

---
+++ src/app/login/find_password/page.tsx
@@ -0,0 +1,12 @@
+import FindPWInput from '@/components/login/FindPW'
+
+export default function Home() {
+    return(
+        <div>
+            <FindPWInput></FindPWInput>
+
+
+            
+        </div>
+    );
+}
