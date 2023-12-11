8c7386acbe7d7d3035ff694005dc716f6765b554
문준호 <wngh46280@gmail.com>
2023-12-11 18:25:23 +0900

    Fix: image -> img로 변경

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -39,7 +39,7 @@ export default function PostCard({
               <div className="relative w-[233.59px] h-[233.59px] overflow-hidden hover:border-2 hover:border-main-color rounded-md">
                 {/* 대표 이미지 쉐도우 */}
                 <div className="absolute inset-0 top-1/2 bg-gradient-to-t from-gray-400 via-transparent to-transparent z-10 opacity-40"></div>
-                <Image
+                <img
                   className="object-cover w-full h-full"
                   src={
                     thumbnail !== null ? thumbnail : "/img/reptimate_logo.png"
@@ -52,7 +52,7 @@ export default function PostCard({
               </div>
               {/* 작성자 정보 */}
               <div className="absolute bottom-0 left-0 mb-2 flex items-center z-20">
-                <Image
+                <img
                   className="ml-2 w-8 h-8 rounded-full border-2"
                   width={10}
                   height={10}
@@ -80,7 +80,7 @@ export default function PostCard({
                 {/* 대표 이미지 쉐도우 */}
                 <div className="absolute inset-0 top-1/2 bg-gradient-to-t from-gray-400 via-transparent to-transparent z-10 opacity-40"></div>
                 <div style={imgStyle}>
-                  <Image
+                  <img
                     className="object-cover absolute inset-0 w-full h-full"
                     width={183.5}
                     height={183.5}
@@ -94,7 +94,7 @@ export default function PostCard({
                 {/* 작성자 정보 */}
                 <div className="absolute bottom-0 left-0 mb-1 flex items-center z-20">
                   {/* 작성자 프로필 이미지 */}
-                  <Image
+                  <img
                     className="ml-1 rounded-full border-2 object-cover"
                     width={20}
                     height={20}
--- src/components/auction/AucutionPostCard.tsx
+++ src/components/auction/AucutionPostCard.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import { Auction } from "@/service/my/auction";
 import Image from "next/image";
 import Link from "next/link";
@@ -59,7 +60,7 @@ export default function AuctionPostCard({
               <div className="relative w-[233.59px] h-[233.59px] overflow-hidden hover:border-2 hover:border-main-color rounded-md">
                 {/* 대표 이미지 쉐도우 */}
                 <div className="absolute inset-0 top-1/2 bg-gradient-to-t from-gray-400 via-transparent to-transparent z-10 opacity-40"></div>
-                <Image
+                <img
                   className="object-cover w-full h-full"
                   src={
                     thumbnail !== null ? thumbnail : "/img/reptimate_logo.png"
@@ -72,7 +73,7 @@ export default function AuctionPostCard({
               </div>
               {/* 작성자 정보 */}
               <div className="absolute bottom-0 left-0 mb-2 flex items-center z-20">
-                <Image
+                <img
                   className="ml-2 w-8 h-8 rounded-full border-2"
                   width={10}
                   height={10}
@@ -113,7 +114,7 @@ export default function AuctionPostCard({
                 {/* 대표 이미지 쉐도우 */}
                 <div className="absolute inset-0 top-1/2 bg-gradient-to-t from-gray-400 via-transparent to-transparent z-10 opacity-40"></div>
                 <div style={imgStyle}>
-                  <Image
+                  <img
                     className="object-cover absolute inset-0 w-full h-full"
                     width={183.5}
                     height={183.5}
@@ -127,7 +128,7 @@ export default function AuctionPostCard({
                 {/* 작성자 정보 */}
                 <div className="absolute bottom-0 left-0 mb-1 flex items-center z-20">
                   {/* 작성자 프로필 이미지 */}
-                  <Image
+                  <img
                     className="ml-1 rounded-full border-2 object-cover"
                     width={20}
                     height={20}
