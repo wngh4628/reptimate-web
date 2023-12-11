938bb60873d7dff6a8f63121db9518053057f814
문준호 <wngh46280@gmail.com>
2023-12-11 18:55:12 +0900

    Fix: 김지훈씨 작업물 마지

--- src/app/community/adoption/edit/[idx]/page.tsx
+++ src/app/community/adoption/edit/[idx]/page.tsx
@@ -1,12 +1,8 @@
 "use client";
 
 import CommunityMenu from "@/components/CommunityMenu";
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
 import React from "react";
 import AdoptionEdit from "@/components/adoption/AdoptionEdit";
-import { TouchBackend } from "react-dnd-touch-backend";
-import { Mobile, PC } from "@/components/ResponsiveLayout";
 
 export default function AdoptionEditPage() {
   return (
