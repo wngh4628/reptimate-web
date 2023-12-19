c4f3f218b44ef6da943ae30a4292f475bd3d0050
문준호 <wngh46280@gmail.com>
2023-12-19 23:34:15 +0900

    Fix: 푸터 내용 추가

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,8 +6,20 @@ export default function Footer() {
 
   if (pathName.startsWith("/streamhost")) return null;
   return (
-    <p className="mt-10 py-11 text-center bg-gray-100">
+    <div className="mt-10 py-11 text-center bg-gray-100">
+      <p className="">
+        {"대표자명: 문준호"}
+      </p>
+      <p className="">
+        {"주소: 서울 동작구 사당로16길 7"}
+      </p>
+      <p className="">
+        {"대표 메일: ioteamnova@gmail.com"}
+      </p>
+      <p className="">
       {"Copyright©ReptiMate All rights reserved."}
-    </p>
+      </p>
+    </div>
+    
   );
 }
