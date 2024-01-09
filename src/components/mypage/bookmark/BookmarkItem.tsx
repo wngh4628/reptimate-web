1b96fec0f9dbec82e2d43a2bb66650fe7b4f5933
rlaryd <rlaryd1822@gmail.com>
2024-01-09 15:21:49 +0900

    fix : 마이페이지 목록 아이템 레이아웃 변경

--- src/components/mypage/bookmark/BookmarkItem.tsx
+++ src/components/mypage/bookmark/BookmarkItem.tsx
@@ -1,5 +1,6 @@
 import { BookmarkBoard } from "@/service/my/bookmark";
 import Link from "next/link";
+import { Mobile, PC } from "../../ResponsiveLayout";
 
 type Props = { post: BookmarkBoard };
 export default function BoardItem({
@@ -20,29 +21,51 @@ export default function BoardItem({
     boardCategory,
   },
 }: Props) {
+  const createdAtFormatted = `${createdAt.getFullYear().toString().slice(2)}.${(
+    createdAt.getMonth() + 1
+  )
+    .toString()
+    .padStart(2, "0")}.${createdAt
+    .getDate()
+    .toString()
+    .padStart(2, "0")}`
+
   return (
     <div className="w-full h-[60px] p-2 border-gray-200 border-[1px]">
       {category === "auction" ? 
       <Link href={`/auction/posts/${idx}`}>
       <div className="w-full flex">
-        <h3 className="font-bold text-xl mx-1">{title}</h3>
+        <PC> 
+          <h3 className="flex font-bold text-xl mx-1 w-4/5">{title}</h3>
+        </PC>
+        <Mobile>
+          <h3 className="flex font-bold text-base mx-1 w-3/5">{title}</h3>
+        </Mobile>
 
-        <div className="flex items-center mt-1 ml-auto">
-          {/* 조회수 */}
-          <img className="flex w-4 mx-1" src="/img/eye.png" />
-          <p className="mr-[5px]">{view}</p>
+        <PC>
+          <div className="flex items-center mt-1 ml-auto">
+            {/* 조회수 */}
+            <img className="flex w-4 mx-1" src="/img/eye.png" />
+            <p className="mr-[5px]">{view}</p>
 
-          {/* 작성일 */}
-          <img className="flex w-3 mx-1" src="/img/clock.png" />
-          <p className="">{`${createdAt.getFullYear().toString().slice(2)}.${(
-            createdAt.getMonth() + 1
-          )
-            .toString()
-            .padStart(2, "0")}.${createdAt
-            .getDate()
-            .toString()
-            .padStart(2, "0")}`}</p>
-        </div>
+            {/* 작성일 */}
+            <img className="flex w-3 mx-1" src="/img/clock.png" />
+            <p className="">{createdAtFormatted}</p>
+          </div>
+        </PC>
+        <Mobile>
+          <div className="flex items-center mt-1 ml-auto">
+            {/* 조회수 */}
+            <img className="flex w-4 mx-1" src="/img/eye.png" />
+            <p className="mr-[5px]">{view}</p>
+
+            {/* 작성일 */}
+            <img className="flex w-3 mx-1" src="/img/clock.png" />
+            <p className="">{createdAtFormatted}</p>
+          </div>
+        </Mobile>
+
+        
       </div>
     </Link>
      :
